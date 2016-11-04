34bca86b550b6f79660dd6501401dd87c991cb16
Miri Kim <miri.kim@onthebeach.co.uk>
2016-11-04 13:51:42 +0000

    Spike; Do not show hidden edges

--- cube.rb
+++ cube.rb
@@ -1,5 +1,7 @@
 require 'gosu'
+
 require_relative 'display'
+require_relative 'vector'
 require_relative 'transformation/projection'
 require_relative 'transformation/rotation'
 require_relative 'transformation/coordinate'
@@ -15,14 +17,23 @@ class Cube
     [2, 6], [6, 7], [3, 7], [0, 4],
     [1, 5], [4, 5], [5, 7], [4, 6]
   ]
+  @@surfaces = [
+    [1, 3, 2, 0],
+    [0, 2, 6, 4],
+    [4, 6, 7, 5],
+    [5, 7, 3, 1],
+    [4, 5, 1, 0],
+    [7, 6, 2, 3]
+  ]
 
   def initialize
     @vertices = [-@@l, @@l].repeated_permutation(3).to_a
     @perspective = false
+    @view_vec = Vec.new([0, 0, -1])
   end
 
   def draw
-    draw_vertices
+    #draw_vertices
     draw_edges
   end
 
@@ -51,7 +62,13 @@ class Cube
   end
 
   def draw_edges
-    @@edges.each { |e| Display::Draw::line(vertices_to_render[e[FROM]], vertices_to_render[e[TO]]) }
+    @@edges.each do |e|
+      surfaces_to_render.each do |surface|
+        if surface.include?(e[FROM]) && surface.include?(e[TO])
+          Display::Draw::line(vertices_to_render[e[FROM]], vertices_to_render[e[TO]])
+        end
+      end
+    end
   end
 
   def vertices_to_render
@@ -62,5 +79,21 @@ class Cube
   def vertices_projected
     # apply perspective/orthographic projection
     @vertices.map { |v| projection.matrix(v) * v }
+  end
+
+  def surfaces_to_render
+    @@surfaces.select do |surface|
+      s1, s2, s3, s4 = surface
+      p1 = vertices_projected[s1]
+      p2 = vertices_projected[s2]
+      p3 = vertices_projected[s3]
+      p4 = vertices_projected[s4]
+      v1 = Vec.new(p1) - Vec.new(p2)
+      v2 = Vec.new(p2) - Vec.new(p3)
+      _n = v1.outer_prod(v2)
+      _c = _n.inner_prod(@view_vec)
+      _c > 0
+      # or n * view_vec > 0
+    end
   end
 end
